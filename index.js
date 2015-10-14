07c35d416efc5b10dfcff4c860f31e4f3213d7f0
lwdos <1127622940@qq.com>
2015-10-14 11:21:48 +0800

    fix link

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-require('./http.js').run(process.argv);
+var tinyHttp = require('tiny-http');
+var File = require('./libs/file.js');
+
+tinyHttp.preHandle = function(req, res) {
+    if (require('url').parse(req.url).path === '/xloader.js') {
+        res.writeHead('200', 'text/javascript');
+        res.end(File.read(require('path').join(__dirname, 'vendor/xloader.js')));
+        return false;
+    }
+};
+
+tinyHttp.middleHandle = function(content, conf) {
+    return content.toString().replace(/<\/body>/i, '<script type="text/javascript" charset="utf-8" src="/xloader.js"></script>\r\n</body>');
+};
+//console.log(tinyHttp)
+var conf = tinyHttp.run(process.argv).conf;
+require('./libs/livereload').checkReload(conf.WEB_ROOT);
+/*
+process.on('uncaughtException', function(e) {
+    console.log(e.stack);
+});
+*/
