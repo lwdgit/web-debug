67b69b1e845101c842f92e9f94b64c3065a08aa7
wenli.lw <wenli.lw.@alibaba-inc.com>
2016-01-30 19:22:50 +0800

    add lost file

---
+++ libs/hateip.js
@@ -0,0 +1,15 @@
+/**
+ * Hateip
+ */
+var hateip = require('hateip');
+exports.init = function(subname) {
+    require('child_process').exec(__dirname + '/../node_modules/hateip/bin/run --name ' + subname, function(err, stdout, stderr) {
+        if (err || stderr) {
+            console.log(err.stack || err || stderr);
+        } else if (stdout) {
+            console.log(stdout);
+        }
+    });
+};
+
+
